a8ff8db4e8311c879a88654e5d0467b4a1e62375
maheshchaudhary <maheshchaudhary845@gmail.com>
2025-12-05 13:16:30 +0530

    Day-15 JavaScript Some Operator

---
+++ Day-15/index.js
@@ -0,0 +1,24 @@
+// let a = "2";
+// let b = "5";
+
+// console.log(a & b);
+// console.log(a | b);
+// console.log(a ^ b);
+// console.log(~b);
+
+// let str = "Hello";
+// console.log(str += "!");
+
+// let c = (1 + 2, 3 + 4);
+// console.log(c);
+
+let arr = [1, 2, 3, 5];
+// arr.push(4, 6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+arr.unshift(0, 9);
+console.log(arr);
+arr.shift();
+console.log(arr);
