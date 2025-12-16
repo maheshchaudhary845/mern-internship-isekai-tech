cc8363b5c0c8ec5152d8e3da8fbcb3c8acf692b6
maheshchaudhary <maheshchaudhary845@gmail.com>
2025-12-16 13:36:56 +0530

    Day-21 JavaScript DOM Exercise Completed

--- Day-21/Exercise 1/script.js
+++ Day-21/Exercise 1/script.js
@@ -39,27 +39,95 @@ let divContainer = document.createElement("div");
 divContainer.classList.add("divContainer");
 document.querySelector(".container").insertAdjacentElement("afterend", divContainer);
 function create() {
+    createButton.disabled = true;
     if (selectTag.value === option1.textContent) {
+        let style = "";
+
         let div = document.createElement("div");
         div.classList.add("divElement")
+
+        let onDiv = document.createElement("div");
+        onDiv.classList.add("onScreen");
+        document.querySelector(".divContainer").append(onDiv);
+
+        let upperDiv = document.createElement("div");
+        upperDiv.classList.add("upper-div");
+        document.querySelector(".onScreen").append(upperDiv);
 
         let input = document.createElement("input");
         input.type = "text";
         input.placeholder = "What do you want to put inside the div";
+        document.querySelector(".upper-div").append(input);
+
+        let lowerDiv = document.createElement("div");
+        lowerDiv.classList.add("lower-div");
+        document.querySelector(".onScreen").append(lowerDiv);
+
+        let addCss = document.createElement("button");
+        addCss.classList.add("add-css");
+        addCss.textContent = "Add CSS";
+        document.querySelector(".lower-div").append(addCss);
+
+        let showCss = document.createElement("div");
+        showCss.classList.add("show-css");
+        document.querySelector(".onScreen").append(showCss);
+        let h4 = document.createElement("h4");
+        h4.textContent = "Applied CSS";
+        document.querySelector(".show-css").append(h4);
+
+
+        addCss.addEventListener("click", function () {
+            addCss.disabled = true;
+            let cssProperty = document.createElement("input");
+            cssProperty.type = "text";
+            cssProperty.placeholder = "Enter CSS Property";
+
+            let cssValue = document.createElement("input");
+            cssValue.type = "text";
+            cssValue.placeholder = "Enter Value of  CSS property";
+
+            document.querySelector(".lower-div").append(cssProperty);
+            document.querySelector(".lower-div").append(cssValue);
+
+            let setCss = document.createElement("button");
+            setCss.classList.add("set-css");
+            setCss.textContent = "Set CSS";
+            document.querySelector(".lower-div").append(setCss);
+            setCss.addEventListener("click", function () {
+                style += `${cssProperty.value}: ${cssValue.value};`;
+
+                let showedCss = `${cssProperty.value}: ${cssValue.value};`;
+                let cssCode = document.createElement("code");
+                cssCode.textContent += showedCss;
+                document.querySelector(".show-css").append(cssCode);
+
+                document.querySelector(".lower-div").removeChild(cssProperty);
+                document.querySelector(".lower-div").removeChild(cssValue);
+                document.querySelector(".lower-div").removeChild(setCss);
+                addCss.disabled = false;
+            })
+
+        })
+
+
         let set = document.createElement("button");
-        set.textContent = "Set";
-        document.querySelector(".divContainer").append(input);
-        document.querySelector(".divContainer").append(set);
+        set.textContent = "Done";
+        document.querySelector(".show-css").insertAdjacentElement("afterend", set);
         set.addEventListener("click", function () {
             div.textContent = input.value;
+            div.style = style;
             document.querySelector(".divContainer").append(div);
-            document.querySelector(".divContainer").removeChild(input);
-            document.querySelector(".divContainer").removeChild(set);
+            document.querySelector(".divContainer").removeChild(onDiv);
+            createButton.disabled = false;
         })
     }
 
     if (selectTag.value === option2.textContent) {
         let img = document.createElement("img");
+
+        let onDiv = document.createElement("div");
+        onDiv.classList.add("onScreen");
+        document.querySelector(".divContainer").append(onDiv);
 
         let input = document.createElement("input");
         input.type = "text";
@@ -74,10 +142,10 @@ function create() {
         let set = document.createElement("button");
         set.textContent = "Set";
 
-        document.querySelector(".divContainer").append(input);
-        document.querySelector(".divContainer").append(inputWidth);
-        document.querySelector(".divContainer").append(inputHeight);
-        document.querySelector(".divContainer").append(set);
+        document.querySelector(".onScreen").append(input);
+        document.querySelector(".onScreen").append(inputWidth);
+        document.querySelector(".onScreen").append(inputHeight);
+        document.querySelector(".onScreen").append(set);
 
         set.addEventListener("click", function () {
             img.src = input.value;
@@ -86,17 +154,20 @@ function create() {
             img.style = "object-fit: cover; object-position: center;";
 
             document.querySelector(".divContainer").append(img);
-            document.querySelector(".divContainer").removeChild(input);
-            document.querySelector(".divContainer").removeChild(inputWidth);
-            document.querySelector(".divContainer").removeChild(inputHeight);
-            document.querySelector(".divContainer").removeChild(set);
+            document.querySelector(".divContainer").removeChild(onDiv);
+            createButton.disabled = false;
         })
     }
 
     if (selectTag.value === option3.textContent) {
+        let onDiv = document.createElement("div");
+        onDiv.classList.add("onScreen");
+        document.querySelector(".divContainer").append(onDiv);
+
         let input = document.createElement("input");
         input.type = "text";
 
         document.querySelector(".divContainer").append(input);
+        createButton.disabled = false;
     }
 }
