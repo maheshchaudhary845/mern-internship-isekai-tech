c9378e0708a96ebe6fd36996f246aa5e157bdc5d
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-17 13:18:22 +0530

    added api to get the single product or by an id

--- E-commerce/backend/routes/productRoutes.js
+++ E-commerce/backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getProducts, createProduct, updateProduct, deleteProduct} = require('../controllers/productControllers');
+const {getProducts, createProduct, updateProduct, deleteProduct, getProductById} = require('../controllers/productControllers');
 const {auth, roleAuth} = require('../middlewares/auth')
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductById);
 router.post('/add', auth, createProduct);
 router.put('/:id', auth, updateProduct);
 router.delete('/:id', auth, deleteProduct);
