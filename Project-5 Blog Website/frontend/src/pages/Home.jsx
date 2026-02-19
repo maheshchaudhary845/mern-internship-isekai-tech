9c0647f58c341bba49b0a6818c3c40ff0488cce0
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-19 14:49:55 +0530

    added recent posts with layout designing

--- Project-5 Blog Website/frontend/src/pages/Home.jsx
+++ Project-5 Blog Website/frontend/src/pages/Home.jsx
@@ -1,8 +1,24 @@
-function Home(){
-    return(
+function Home() {
+    return (
         <>
             <h2>Recent Posts</h2>
-            
+            <div className="posts">
+                <div className="post">
+                    <div className="img-cont">
+                        <img src="https://preview.colorlib.com/theme/webmag/img/post-3.avif" alt="" />
+                    </div>
+                    <div className="meta-cont">
+                        <p className="category">JavaScript</p>
+                        <p className="date-time">Febuary 19, 2026</p>
+                    </div>
+                    <div className="content">
+                        <h3>Title</h3>
+                        <p className="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum exercitationem iste cum temporibus velit delectus alias, fuga ut atque vitae!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
