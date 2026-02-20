9796f8ce7852ac49ef83400613049aeef449a5eb
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-20 14:53:53 +0530

    adding category collection

--- Project-5 Blog Website/backend/models/Post.js
+++ Project-5 Blog Website/backend/models/Post.js
@@ -13,6 +13,9 @@ const postSchema = new mongoose.Schema({
         type: String,
         // required: true
     },
+    // category: {
+    //     type: 
+    // },
     author:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
