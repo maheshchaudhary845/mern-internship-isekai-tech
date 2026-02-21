cf83bef43b207756d6eb21aff252cb6df7ad0084
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-22 01:19:38 +0530

    added role auth

--- Project-5 Blog Website/backend/middlewares/authMiddleware.js
+++ Project-5 Blog Website/backend/middlewares/authMiddleware.js
@@ -24,3 +24,33 @@ exports.auth = (req, res, next)=>{
     }
     
 }
+
+exports.roleAuth = (role)=>{
+    return(req, res, next)=>{
+        const authHeader = req.header("Authorization");
+        const token = authHeader.split(" ").pop();
+
+        if(!token){
+            return res.status(404).json({
+                success: false,
+                message: "token not found"
+            })
+        }
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            if(decode.role != role){
+                return res.status(403).json({
+                    success: false,
+                    message: "You are not authorized"
+                })
+            }
+            next();
+        }catch(err){
+            res.status(500).json({
+                success: false,
+                message: err.message
+            })
+        }
+
+    }
+}
