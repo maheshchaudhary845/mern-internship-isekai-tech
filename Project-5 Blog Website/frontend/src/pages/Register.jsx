a578ee4c75b30499261ce46ca5a98cdd342c8b52
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-23 00:04:10 +0530

    added logic in register page

--- Project-5 Blog Website/frontend/src/pages/Register.jsx
+++ Project-5 Blog Website/frontend/src/pages/Register.jsx
@@ -1,20 +1,52 @@
-import { Link } from "react-router";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+
 
 function Register() {
+    const [form, setForm] = useState({ firstName: "", lastName: "", email: "", password: "" });
+    const navigate = useNavigate();
+
+    const handleForm = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const res = await fetch("http://localhost:3000/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(form)
+            });
+
+            const data = await res.json();
+            const { message } = data;
+            console.log(message);
+            if(data.success){
+                navigate('/login');
+            }
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
     return (
         <div className="bg">
             <div className="auth-box">
                 <h1>REGISTER</h1>
                 <div className="auth">
                     <div className="name">
-                        <input id="firstname" type="text" placeholder="First Name" />
-                        <input id="lastname" type="text" placeholder="Last Name" />
+                        <input id="firstname" name="firstName" type="text" value={form.firstName} onChange={handleForm} placeholder="First Name" />
+                        <input id="lastname" name="lastName" type="text" value={form.lastName} onChange={handleForm} placeholder="Last Name" />
                     </div>
-                    <input type="email" name="email" id="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input type="email" name="email" id="email" value={form.email} onChange={handleForm} placeholder="Email" />
+                    <input type="password" name="password" value={form.password} onChange={handleForm} placeholder="Password" />
                 </div>
                 <div className="button">
-                    <button className="btn-register">Register</button>
+                    <button className="btn-register" onClick={handleSubmit}>Register</button>
                 </div>
                 <div className="login-cont">
                     <p>Already have an account?</p>
