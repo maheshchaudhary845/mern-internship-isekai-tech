7f42a61873eaa5934071ee1d088b2df357e9235b
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-02-26 17:34:11 +0530

    fix cors

--- Project-5 Blog Website/backend/server.js
+++ Project-5 Blog Website/backend/server.js
@@ -16,10 +16,13 @@ const port = 3000;
 connectDB();
 
 app.use(cookieParser())
-app.use(cors({credentials: true}));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use('/uploads', express.static("uploads"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
@@ -28,6 +31,6 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/tags', tagRoutes);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
