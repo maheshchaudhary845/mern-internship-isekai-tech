29d50f77d20a4e84f2da9c423607e44d7bbe5509
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-11 12:15:11 +0530

    add user context provider

--- Project-5 Blog Website/frontend/src/main.jsx
+++ Project-5 Blog Website/frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UserContextProvider from './context/userContext'
 
 document.documentElement.classList.add("dark")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </StrictMode>,
 )
