7830be3ea1deb9bcaab1ba916e3f4045e3880ed1
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-13 17:54:17 +0530

    add search api and rename activePosts to posts

--- Project-5 Blog Website/frontend/src/pages/Dashboard/Dashboard.jsx
+++ Project-5 Blog Website/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -4,28 +4,39 @@ import { Link } from "react-router";
 import { AuthContext } from "@/context/AuthContext";
 
 function Dashboard() {
-    const [activePosts, setActivePosts] = useState([]);
-    const {auth} = useContext(AuthContext);
-    
+    const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState("");
+    const { auth } = useContext(AuthContext);
+
     useEffect(() => {
-        async function fetchPosts() {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts/user/${auth?.id}`);
-            const { success, data, message } = await res.json();
-            if (success) {
-                setActivePosts(data);
-            } else {
-                console.error(message);
+        if (!auth) return;
+
+        const timer = setTimeout(async () => {
+            try {
+                const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts/user/${auth?.id}?search=${search}`);
+                const { data, success, message } = await res.json();
+
+                if (success) {
+                    setPosts(data);
+                }
+                else {
+                    console.error(message);
+                }
+
+            } catch (err) {
+                console.error(err);
             }
-        }
-        if(auth){
-            fetchPosts();
-        }
-        }, [auth])
+        }, 500)
+
+        return ()=> clearTimeout(timer);
+
+    }, [auth, search])
+
     return (
         <>
             <div className="head flex justify-between items-center">
                 <h1>Dashboard</h1>
-                <input className="bg-neutral-800 p-2 rounded-full max-w-sm w-full" type="search" name="search" id="" placeholder="Search Post" />
+                <input className="bg-neutral-800 p-2 rounded-full max-w-sm w-full" value={search} onChange={(e)=>setSearch(e.target.value)} type="search" name="search" placeholder="Search Post" />
                 <Link to={'/createpost'}><button className="bg-green-500 px-2 py-1 rounded-sm cursor-pointer hover:bg-green-600">Create Post +</button></Link>
             </div>
 
@@ -35,8 +46,8 @@ function Dashboard() {
                         <h3>All Posts</h3>
                     </div>
                     <div className="dash-posts">
-                        {activePosts.map(post => (
-                            <DashPost key={post.id} post={post} />
+                        {posts.map(post => (
+                            <DashPost key={post.id} post={post} setPosts={setPosts} />
                         ))}
 
                     </div>
