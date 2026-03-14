458e9db88ee00f15a6a5a7bbd44c087e778b3eb5
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-14 21:14:45 +0530

    create Profile page

---
+++ Project-5 Blog Website/frontend/src/pages/Profile.jsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import Post from "@/components/Post";
+import { Link } from "react-router";
+
+function Profile() {
+    const { auth } = useContext(AuthContext);
+    const [posts, setPosts] = useState([]);
+    const [totalPosts, setTotalPosts] = useState("");
+
+    useEffect(() => {
+        async function fetchPosts() {
+            if (!auth) return;
+
+            try {
+                const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts/user/${auth.id}`);
+                const { data, success, message, total } = await res.json();
+
+                if (success) {
+                    setPosts(data);
+                    setTotalPosts(total);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchPosts();
+    }, [auth])
+
+    if (!auth) {
+        return <p className="text-center">Loading...</p>
+    }
+
+    return (
+        <>
+            <div className="flex flex-col gap-3 items-center max-w-6xl m-auto">
+                <div className="img-container rounded-full w-50 h-50 overflow-hidden">
+                    <img className="object-cover w-full h-full" src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Photos.png" alt="profile pic" />
+                </div>
+                <h4 className="text-xl">{auth?.fullName}</h4>
+                <p className="text-lg">{totalPosts}<span className="text-gray-400"> posts</span></p>
+            </div>
+
+            <div className="posts mt-10!">
+                {posts.map(post => (
+                    <Link to={`/post/${post.slug}`}>
+                        <Post key={post.id} post={post} />
+                    </Link>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Profile;
