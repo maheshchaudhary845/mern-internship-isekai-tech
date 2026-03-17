a4b249697be000b709b03247bf298315099ad001
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-17 14:58:11 +0530

    add timestamps

--- Project-5 Blog Website/backend/models/Comment.js
+++ Project-5 Blog Website/backend/models/Comment.js
@@ -13,6 +13,6 @@ const commentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model("Comment", commentSchema);
