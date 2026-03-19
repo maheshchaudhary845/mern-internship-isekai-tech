ce8cf43d68cdb2788c06fa9b45de1ad4dc3fdaf6
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-19 14:40:49 +0530

    add sorting

--- Project-5 Blog Website/frontend/src/pages/Home.jsx
+++ Project-5 Blog Website/frontend/src/pages/Home.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router";
 
 function Home() {
     const [posts, setPosts] = useState([]);
+    const [sort, setSort] = useState("");
 
     useEffect(() => {
         async function fetchPosts() {
-            const res = await fetch("http://localhost:3000/api/posts");
+            const res = await fetch(`http://localhost:3000/api/posts?sort=${sort}`);
             const { success, data, message } = await res.json();
             if (success) {
                 setPosts(data);
@@ -16,19 +17,26 @@ function Home() {
             }
         }
         fetchPosts();
-    }, [])
+    }, [sort])
+    console.log(sort)
     return (
-        <>
-            
-            <h2>Recent Posts</h2>
+        <div className="px-2">
+            <div className="flex justify-between items-center">
+                <h2>All Posts</h2>
+                <select value={sort} onChange={(e)=>setSort(e.target.value)} className="bg-gray-600 rounded-sm p-2">
+                    <option value="latest">Latest</option>
+                    <option value="popular">Popular</option>
+                    <option value="category">Category</option>
+                </select>
+            </div>
             <div className="posts">
                 {posts.map(post => (
-                <Link to={`/post/${post.slug}`} key={post.id}>
-                    <Post  post={post} />
-                </Link>
+                    <Link to={`/post/${post.slug}`} key={post.id}>
+                        <Post post={post} />
+                    </Link>
                 ))}
             </div>
-        </>
+        </div>
     )
 };
 
