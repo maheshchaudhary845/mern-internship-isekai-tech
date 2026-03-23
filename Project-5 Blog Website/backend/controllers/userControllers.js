295c09fc5ef7fbc62dc487c76ded4218f66601ca
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-23 18:01:34 +0530

    add update user api

--- Project-5 Blog Website/backend/controllers/userControllers.js
+++ Project-5 Blog Website/backend/controllers/userControllers.js
@@ -2,14 +2,14 @@ const User = require("../models/User");
 
 module.exports = {
     async getUsers(req, res) {
-        try{
+        try {
             const users = await User.find();
             res.json({
                 success: true,
                 data: users,
                 message: "Users fetched successfully"
             })
-        }catch(err){
+        } catch (err) {
             res.status(500).json({
                 success: false,
                 message: err.message
@@ -17,8 +17,8 @@ module.exports = {
         }
     },
 
-    async getCurrentUser(req, res){
-        try{
+    async getCurrentUser(req, res) {
+        try {
             const user = await User.findById(req.user.id)
 
             res.json({
@@ -26,12 +26,61 @@ module.exports = {
                 data: user,
                 message: "Current user fetched!"
             })
-        }catch(err){
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            })
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const { firstName, lastName, currentPassword, newPassword } = req.body;
+            const user = await User.findById(req.user.id).select("+password");  
+
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found"
+                })
+            }
+            
+            if (firstName) {
+                user.firstName = firstName;
+            }
+            
+            if (lastName) {
+                user.lastName = lastName;
+            }
+            
+            if (currentPassword && newPassword) {
+                const isMatch = await user.comparePassword(currentPassword);
+                if (!isMatch) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "Invalid current password"
+                    })
+                }
+
+                user.password = newPassword;
+            }
+
+            await user.save();
+
+            res.json({
+                success: true,
+                data: user,
+                message: "User updated successfully"
+            })
+
+
+        } catch (err) {
             res.status(500).json({
                 success: false,
                 message: err.message
             })
         }
     }
-    
+
 }
--- Project-5 Blog Website/backend/routes/userRoutes.js
+++ Project-5 Blog Website/backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getUsers, getCurrentUser} = require('../controllers/userControllers');
+const {getUsers, getCurrentUser, updateUser} = require('../controllers/userControllers');
 const {auth} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', auth, getUsers);
 router.get('/me',auth, getCurrentUser);
+router.put('/update', auth, updateUser);
 
 module.exports = router;
