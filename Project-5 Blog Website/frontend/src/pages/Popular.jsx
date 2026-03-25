bed690904e9144073bd04dcd38fb50cac8fdfc97
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-25 17:05:01 +0530

    set error if exists in ui

--- Project-5 Blog Website/frontend/src/pages/Popular.jsx
+++ Project-5 Blog Website/frontend/src/pages/Popular.jsx
@@ -6,28 +6,32 @@ import { Link } from "react-router";
 function Popular() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
     useEffect(() => {
         async function fetchPosts() {
             try {
                 setLoading(true);
                 const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts?sort=popular`);
-                const { success, data } = await res.json();
+                const { success, data, message } = await res.json();
                 if (success) {
                     setPosts(data);
+                } else {
+                    setError(message)
                 }
             } catch (err) {
                 console.error(err);
-            } finally{
+            } finally {
                 setLoading(false);
             }
         }
         fetchPosts();
     }, [])
 
-    if(loading) return <Loading />
-    
+    if (loading) return <Loading />
+
     return (
         <>
+            {error && <p className="text-center text-gray-300">{error}</p>}
             <div className="posts mx-2! ">
                 {posts.map(post => (
                     <Link key={post._id} to={`/post/${post.slug}`}>
