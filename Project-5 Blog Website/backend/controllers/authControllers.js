2c38808962bf1e8ea93bfe628a06d8fc9507954c
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-25 17:50:17 +0530

    fix cookie on login

--- Project-5 Blog Website/backend/controllers/authControllers.js
+++ Project-5 Blog Website/backend/controllers/authControllers.js
@@ -47,7 +47,7 @@ exports.login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: isProduction,
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
