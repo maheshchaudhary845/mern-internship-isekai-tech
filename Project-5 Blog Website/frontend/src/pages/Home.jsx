e0e9afc98208fb8984bb4cb5981c982e7962e913
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-25 13:13:10 +0530

    enhance ui on tags

--- Project-5 Blog Website/frontend/src/pages/Home.jsx
+++ Project-5 Blog Website/frontend/src/pages/Home.jsx
@@ -76,7 +76,7 @@ function Home() {
             {loading && <Loading />}
             <div className="px-2">
                 <h2>All Posts</h2>
-                <div className="flex gap-3 mt-4 items-center">
+                <div className="flex flex-wrap gap-3 mt-4 items-center">
                     <select value={filters.sort} onChange={(e) => setFilters(prev => ({ ...prev, sort: e.target.value }))} className="bg-gray-600 rounded-sm p-2">
                         <option value="latest">Latest</option>
                         <option value="popular">Popular</option>
@@ -90,18 +90,21 @@ function Home() {
                     <input type="date" onChange={(e) => setFilters(prev => ({ ...prev, from: e.target.value }))} className="bg-gray-700 px-3 py-2 rounded-md" />
                     <input type="date" onChange={(e) => setFilters(prev => ({ ...prev, to: e.target.value }))} className="bg-gray-700 px-3 py-2 rounded-md" />
                 </div>
-                <div className="flex my-scroll-container overflow-x-auto gap-2 my-3">
-                    {tags.map(tag => (
-                        <button key={tag._id}
-                            onClick={() => {
-                                setFilters(prev => ({
-                                    ...prev,
-                                    tags: prev.tags.includes(tag.slug) ? prev.tags.filter(t => t !== tag.slug) : [...prev.tags, tag.slug]
-                                }))
-                            }}
-                            className={`px-3 py-0 rounded-full text-sm border ${filters.tags.includes(tag.slug) ? "bg-gray-600 border-gray-600" : "bg-sky-600 border-sky-600"}`}>#{tag.name}</button>
-                    ))}
+                <div className="relative ball overflow-hidden">
+                    <div className="flex my-scroll-container overflow-x-auto gap-2 my-3">
+                        {tags.map(tag => (
+                            <button key={tag._id}
+                                onClick={() => {
+                                    setFilters(prev => ({
+                                        ...prev,
+                                        tags: prev.tags.includes(tag.slug) ? prev.tags.filter(t => t !== tag.slug) : [...prev.tags, tag.slug]
+                                    }))
+                                }}
+                                className={`px-3 py-0 rounded-full text-sm border cursor-pointer ${filters.tags.includes(tag.slug) ? "bg-gray-600 border-gray-600" : "bg-sky-600 border-sky-600"}`}>#{tag.name}</button>
+                        ))}
+                    </div>
                 </div>
+
                 {error ? <p className="text-center">{error}</p> :
                     <div className="posts">
                         {posts.map(post => (
