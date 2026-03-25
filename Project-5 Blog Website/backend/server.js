7814e3be6bbab707e0687f62d4ed8aa8dc684647
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-03-25 16:52:50 +0530

    fix origin of cors

--- Project-5 Blog Website/backend/server.js
+++ Project-5 Blog Website/backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://mern-internship-isekai-tech.vercel.app/"
+        "https://mern-internship-isekai-tech.vercel.app"
     ],
     credentials: true
 }));
